706320bcc6ce3fac4363b98dbe74ddb80f0da17e
use-the-fork <sincore@gmail.com>
2024-05-13 18:12:54 -0400

    Added new modules and configurations for sushi workstation setup
    📁 - style.nix:
    • Added new file with config for GTK and QT themes, icons, and font
    • Set style to use Kvantum and force GTK
    
    📁 - device.nix:
    • Added new file defining device characteristics for a laptop with AMD CPU and GPU
    
    📁 - disko.nix:
    • Created new file for setting up file systems and partitions with specific mountpoints and options
    
    📁 - modules.nix:
    • Created new file to enable iso, headless, graphical, workstation and laptop modules
    
    📁 - profiles.nix:
    • Created new file to enable workstation profile
    
    📁 - system.nix:
    • Added new file defining main system settings, including boot, security, networking, services, and program configs
    
    📁 - usrEnv.nix:
    • Added new file setting user environment to Hyprland and enabling swaylock
    
    📁 - default.nix:
    • Created new file to import all modules, set up home-manager and define host name
    
    📁 - hardware-configuration.nix:
    • Created new file with generated configurations for hardware
    
    Signed-off-by: use-the-fork <sincore@gmail.com>

---
+++ systems/x86_64-linux/sushi/modules/device.nix
@@ -0,0 +1,11 @@
+{
+  my.device = {
+    type = "laptop";
+    cpu.type = "amd";
+    gpu.type = "amd";
+    monitors = ["eDP-1"];
+    hasBluetooth = false;
+    hasSound = true;
+    hasTPM = true;
+  };
+}
