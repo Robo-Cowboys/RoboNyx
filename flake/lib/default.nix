b4677dcb186a4f76a67641df06e3c659d00c22f5
use-the-fork <sincore@gmail.com>
2024-05-16 21:04:43 -0400

    📂 [nyx/lib/default.nix]: • Deleted complete file
    📄 [nyx/flake/lib/common.nix]:
    • New file created with function 'import' that allows manual imports
    
    📄 [nyx/flake/lib/default.nix]:
    • New file created to organize several utility functions
    
    📄 [nyx/flake/lib/hardware.nix]:
    • New file created to check host platform and get primary monitor from a list of monitors
    
    📄 [nyx/flake/lib/helpers/default.nix]:
    • New file created encompassing systemd, fs, types, themes, modules helper functions
    
    📄 [nyx/flake/lib/aliases.nix]:
    • New file with bcm sslTemplate and common shellColors
    
    📄 [nyx/flake/lib/builders.nix]:
    • New file created with mkSystem, mkNixosSystem, mkNixosIso, mkRaspi4Image functions
    
    📄 [nyx/flake/lib/helpers/systemd.nix]:
    • New file created with hardenService function
    
    📄 [nyx/flake/lib/helpers/fs.nix]:
    • New file created with function to get secret file, shared module and ssh key files
    
    📄 [nyx/flake/modules/default.nix]:
    • New file created to expose nixosModules and homeManagerModules by the flake
    
    📄 [nyx/flake/args.nix]:
    • New file created to append user groups if they exist in config.users.groups
    
    📄 [flake/keys.nix]:
    • New file created to handle public keys
    
    📄 [nyx/flake/iso-images.nix]:
    • New file created to handle the creation of ISO images
    
    📄 [nyx/flake/fmt.nix]:
    • New file created to handle formatting settings like the formatter for `nix fmt`
    
    📄 [nyx/flake/lib/services.nix]:
    • New file created with mkGraphicalService and mkHyprlandService functions
    
    📄 [nyx/flake/lib/validators.nix]:
    • New file created with several validation functions
    
    📄 [nyx/flake/shell.nix]:
    • New file created for setting up the dev shell
    
    🔄 [nyx/flake/lib/helpers/types.nix and nyx/flake/lib/helpers/themes.nix]:
    • Files moved to new location

---
+++ flake/lib/default.nix
@@ -0,0 +1,40 @@
+{inputs, ...}: let
+  inherit (inputs.nixpkgs) lib;
+  inherit (lib) foldl recursiveUpdate;
+  inherit (import ./common.nix {inherit lib;}) import';
+
+  # helpful utility functions used around the system
+  builders = import' ./builders.nix {inherit inputs;}; # system builders
+  services = import' ./services.nix; # systemd-service generators
+  validators = import' ./validators.nix; # validate system conditions
+  helpers = import' ./helpers; # helper functions
+  hardware = import' ./hardware.nix; # hardware capability checks
+  xdg = import' ./xdg; # xdg user directories & templates
+
+  # abstractions over networking functions
+  # dag library is a modified version of the one found in
+  # rycee's NUR repository
+  dag = import' ./network/dag.nix; # dag is in network because it's designed for network only use
+  firewall = import' ./network/firewall.nix {inherit dag;}; # build nftables tables and chains
+  namespacing = import' ./network/namespacing.nix; # TODO
+
+  # aliases for commonly used strings or functions
+  aliases = import' ./aliases.nix;
+
+  importedLibs = [builders services validators helpers hardware aliases firewall namespacing dag xdg];
+  extendedLib = lib.extend (_: _: foldl recursiveUpdate {} importedLibs);
+in {
+  perSystem = {
+    # set the `lib` arg of the flake as the extended lib. If I am right, this should
+    # override the previous argument (i.e. the original nixpkgs.lib) with my own
+    # which is the nixpkgs library, but with my own custom actions.
+    imports = [{_module.args.lib = extendedLib;}];
+  };
+
+  flake = {
+    # also set `lib` as a flake output, which allows for it to be referenced outside
+    # the scope of this flake. This is useful for when I want to refer to my extended
+    # library from outside this flake.
+    lib = extendedLib;
+  };
+}
