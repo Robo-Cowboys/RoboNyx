d7ac7506d65d172479b10719fd940f3575946be7
use-the-fork <sincore@gmail.com>
2024-05-19 12:13:36 -0400

    📝 README.md, hosts/default.nix, flake.nix, homes/sincore/default.nix, and nyx/flake/modules/default.nix: • Replaced 'nyx' with 'robo-nyx' in import paths • Updated the 'robo-nyx' URL to the github link in flake.nix • Removed 'nyx.cachix.org' from extra-substituters and extra-trusted-public-keys in flake.nix

--- flake/modules/default.nix
+++ flake/modules/default.nix
@@ -7,7 +7,7 @@ in {
   flake = {
     # set of modules exposed by my flake to be consumed by others
     # those can be imported by adding this flake as an input and then importing the nixosModules.<moduleName>
-    # i.e imports = [ inputs.nyx.nixosModules.steam-compat ]; or modules = [ inputs.nyx.nixosModules.steam-compat ];
+    # i.e imports = [ inputs.robo-nyx.nixosModules.steam-compat ]; or modules = [ inputs.robo-nyx.nixosModules.steam-compat ];
     nixosModules = {
       # we do not want to provide a default module
       default = builtins.throw "There is no default module, sorry!";
