4f780d2af649d72a611e4ec0888d6c792fbbb820
use-the-fork <sincore@gmail.com>
2024-05-19 10:43:12 -0400

    📁 [.envrc]: • Removed monitoring of flake directory for changes • Disabled remote builders for trivial flake actions
    📁 [flake.lock]:
    • Updated the last modified time and narHash of several components
    • Added new dependencies such as 'just-flake' and 'pre-commit-hooks-nix_2'
    
    📁 [flake.nix]:
    • Added the 'just-flake' and 'pre-commit-hooks-nix' modules with their respective URL and follows properties
    
    📁 [justfile]:
    • Created file that imports 'just-flake.just' and displays a list of recipes
    
    📁 [nyx/flake/shell.nix]:
    • Updated the 'treefmt.config' and enabled multiple programs such as 'alejandra', 'black', 'deadnix', etc
    • Updated the excludes in the 'pre-commit' settings
    • Added the 'config.just-flake.outputs.devShell' dependency in 'devShells.default' and included it in packages as well.

--- flake/shell.nix
+++ flake/shell.nix
@@ -1,45 +1,65 @@
-{ inputs, config, pkgs, lib, system, ... }: {
+{
+  inputs,
+  ...
+}: {
   imports = [
-           inputs.treefmt-nix.flakeModule
-            inputs.just-flake.flakeModule
-            inputs.pre-commit-hooks-nix.flakeModule
+    inputs.treefmt-nix.flakeModule
+    inputs.just-flake.flakeModule
+    inputs.pre-commit-hooks-nix.flakeModule
   ];
 
-  perSystem = {pkgs, config, ...}: {
-          just-flake.features = {
-            treefmt.enable = true;
-            convco.enable = true;
-          };
+  perSystem = {
+    pkgs,
+    config,
+    ...
+  }: {
+    just-flake.features = {
+      treefmt.enable = true;
+      convco.enable = true;
+    };
 
-          # Add your auto-formatters here.
-          # cf. https://numtide.github.io/treefmt/
-          treefmt.config = {
-            projectRootFile = "flake.nix";
-            flakeCheck = false; # pre-commit-hooks.nix checks this
-            programs = {
-              nixpkgs-fmt.enable = true;
-            };
-          };
+    # Add your auto-formatters here.
+    # cf. https://numtide.github.io/treefmt/
+    treefmt.config = {
+      #            projectRootFile = "flake.nix";
+      flakeCheck = false; # pre-commit-hooks.nix checks this
+      programs = {
+        alejandra.enable = true;
+        black.enable = true;
+        deadnix.enable = true;
+        statix = {
+          enable = true;
+          disabled-lints = [
+            "manual_inherit_from"
+          ];
+        };
+        mdformat.enable = true;
+        taplo.enable = true;
+        shfmt.enable = true;
+        prettier.enable = true;
+      };
+    };
 
-          pre-commit = {
-            check.enable = true;
-            settings = {
-              hooks = {
-                treefmt.enable = true;
-                convco.enable = true;
-              };
-            };
-          };
+    pre-commit = {
+      check.enable = true;
+      settings = {
+        excludes = ["flake.lock" "r'.+\.age$'" "r'.+\.sh$'"];
+        hooks = {
+          treefmt.enable = true;
+          convco.enable = true;
+        };
+      };
+    };
 
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [
-              config.treefmt.build.devShell
-              config.just-flake.outputs.devShell
-              config.pre-commit.devShell
-            ];
-            packages = [
-              config.pre-commit.settings.tools.convco
-            ];
-          };
+    devShells.default = pkgs.mkShell {
+      inputsFrom = [
+        config.treefmt.build.devShell
+        config.just-flake.outputs.devShell
+        config.pre-commit.devShell
+      ];
+      packages = [
+        config.pre-commit.settings.tools.convco
+      ];
+    };
   };
 }
