da04cbecd06281a88effc0968ef4de862dcc5532
use-the-fork <sincore@gmail.com>
2024-05-20 18:16:59 -0400

    Remove Catppuccin theme from bat config
    The Catppuccin theme was removed from the bat configuration file and the flake.lock file was updated accordingly. The removal of the theme resulted in the change of the "narHash" and "lastModified" values in the flake.lock file.

--- modules/home/programs/terminal/tools/bat.nix
+++ modules/home/programs/terminal/tools/bat.nix
@@ -1,9 +1,4 @@
-{pkgs, ...}: let
-  catppuccin = builtins.readFile (pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme";
-    hash = "sha256-qMQNJGZImmjrqzy7IiEkY5IhvPAMZpq0W6skLLsng/w=";
-  });
-in {
+{pkgs, ...}: {
   programs.bat = {
     enable = true;
     themes = {
