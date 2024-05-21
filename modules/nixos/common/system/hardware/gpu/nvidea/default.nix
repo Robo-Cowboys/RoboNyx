ac77ddec74045d503753adfa2360f9964e82a421
use-the-fork <sincore@gmail.com>
2024-05-21 18:02:46 -0400

    Refactor NVIDIA GPU configuration in NixOS module
    Simplified the NVIDIA package selection in the NixOS configuration by removing the code for checking and comparing stable and beta versions. Also, implemented a structure for the NVIDIA Prime configuration to support offload settings by adding relevant properties and commands. Disabled commented-out code lines have also been reformatted for better readability.

--- modules/nixos/common/system/hardware/gpu/nvidea/default.nix
+++ modules/nixos/common/system/hardware/gpu/nvidea/default.nix
@@ -4,16 +4,9 @@
   lib,
   ...
 }: let
-  inherit (lib) mkIf mkDefault mkMerge versionOlder;
+  inherit (lib) mkIf mkMerge;
 
   # use the latest possible nvidia package
-  nvStable = config.boot.kernelPackages.nvidiaPackages.stable.version;
-  nvBeta = config.boot.kernelPackages.nvidiaPackages.beta.version;
-
-  nvidiaPackage =
-    if (versionOlder nvBeta nvStable)
-    then config.boot.kernelPackages.nvidiaPackages.stable
-    else config.boot.kernelPackages.nvidiaPackages.beta;
 
   dev = config.modules.device;
   env = config.modules.usrEnv;
@@ -35,30 +28,38 @@ in {
         (mkIf env.isWayland {
           WLR_NO_HARDWARE_CURSORS = "1";
         })
-
       ];
       systemPackages = with pkgs; [
-#        nvtopPackages.nvidia
-#
-#        # mesa
-#        mesa
-#
-#        # vulkan
-#        vulkan-tools
-#        vulkan-loader
-#        vulkan-validation-layers
-#        vulkan-extension-layer
-#
-#        # libva
-#        libva
-#        libva-utils
+        #        nvtopPackages.nvidia
+        #
+        #        # mesa
+        #        mesa
+        #
+        #        # vulkan
+        #        vulkan-tools
+        #        vulkan-loader
+        #        vulkan-validation-layers
+        #        vulkan-extension-layer
+        #
+        #        # libva
+        #        libva
+        #        libva-utils
       ];
     };
 
     hardware = {
       nvidia = {
-        # For people who want to use sync instead of offload. Especially for AMD CPU users
-        prime.sync.enable = lib.mkOverride 990 true;
+        prime = {
+          #TODO: @PETE Check This
+          offload = {
+            enable = lib.mkOverride 990 true;
+            enableOffloadCmd = lib.mkIf config.hardware.nvidia.prime.offload.enable true; # Provides `nvidia-offload` command.
+          };
+
+          #TODO: @PETE Check This
+          #amdgpuBusId = "PCI:5:0:0";
+          nvidiaBusId = "PCI:1:0:1";
+        };
       };
 
       opengl = {
