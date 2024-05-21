ab04b014085ddf218dbd6a379c588b750caaa89b
use-the-fork <sincore@gmail.com>
2024-05-21 17:53:44 -0400

    Simplify Nvidia GPU configuration
    This commit simplifies the Nvidia GPU configuration file by removing unused Xorg settings and blacklisted kernel modules. In addition, it replaces the offload setting with prime sync for AMD CPU users and modifies the loaded opengl package. Commented out packages were not removed, but merely commented out for future references.

--- modules/nixos/common/system/hardware/gpu/nvidea/default.nix
+++ modules/nixos/common/system/hardware/gpu/nvidea/default.nix
@@ -26,29 +26,7 @@ in {
       {
         videoDrivers = ["nvidia"];
       }
-
-      # xorg settings
-      (mkIf (!env.isWayland) {
-        # disable DPMS
-        monitorSection = ''
-          Option "DPMS" "false"
-        '';
-
-        # disable screen blanking in general
-        serverFlagsSection = ''
-          Option "StandbyTime" "0"
-          Option "SuspendTime" "0"
-          Option "OffTime" "0"
-          Option "BlankTime" "0"
-        '';
-      })
     ];
-
-    # blacklist nouveau module so that it does not conflict with nvidia drm stuff
-    # also the nouveau performance is godawful, I'd rather run linux on a piece of paper than use nouveau
-    # no offense to nouveau devs, I'm sure they're doing their best and they have my respect for that
-    # but their best does not constitute a usable driver for me
-    boot.blacklistedKernelModules = ["nouveau"];
 
     environment = {
       sessionVariables = mkMerge [
@@ -56,61 +34,35 @@ in {
 
         (mkIf env.isWayland {
           WLR_NO_HARDWARE_CURSORS = "1";
-          #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-          #GBM_BACKEND = "nvidia-drm"; # breaks firefox apparently
         })
 
-        (mkIf (env.isWayland && (dev.gpu == "hybrid-nv")) {
-          #__NV_PRIME_RENDER_OFFLOAD = "1";
-          #WLR_DRM_DEVICES = mkDefault "/dev/dri/card1:/dev/dri/card0";
-        })
       ];
       systemPackages = with pkgs; [
-        nvtopPackages.nvidia
-
-        # mesa
-        mesa
-
-        # vulkan
-        vulkan-tools
-        vulkan-loader
-        vulkan-validation-layers
-        vulkan-extension-layer
-
-        # libva
-        libva
-        libva-utils
+#        nvtopPackages.nvidia
+#
+#        # mesa
+#        mesa
+#
+#        # vulkan
+#        vulkan-tools
+#        vulkan-loader
+#        vulkan-validation-layers
+#        vulkan-extension-layer
+#
+#        # libva
+#        libva
+#        libva-utils
       ];
     };
 
     hardware = {
       nvidia = {
-        package = mkDefault nvidiaPackage;
-        modesetting.enable = mkDefault true;
-
-        prime.offload = let
-          isHybrid = dev.gpu == "hybrid-nv";
-        in {
-          enable = isHybrid;
-          enableOffloadCmd = isHybrid;
-        };
-
-        powerManagement = {
-          enable = mkDefault true;
-          finegrained = mkDefault false;
-        };
-
-        # use open source drivers by default, hosts may override this option if their gpu is
-        # not supported by the open source drivers
-        open = mkDefault true;
-        nvidiaSettings = false; # add nvidia-settings to pkgs, useless on nixos
-        nvidiaPersistenced = true;
-        forceFullCompositionPipeline = true;
+        # For people who want to use sync instead of offload. Especially for AMD CPU users
+        prime.sync.enable = lib.mkOverride 990 true;
       };
 
       opengl = {
-        extraPackages = with pkgs; [nvidia-vaapi-driver];
-        extraPackages32 = with pkgs.pkgsi686Linux; [nvidia-vaapi-driver];
+        extraPackages = with pkgs; [vaapiVdpau];
       };
     };
   };
