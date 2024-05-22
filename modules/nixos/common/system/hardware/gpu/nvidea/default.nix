c918b4936165a8be43ef0063766824f8ccb9de24
use-the-fork <sincore@gmail.com>
2024-05-21 21:00:00 -0400

    Update bus IDs for GPU modules
    The bus IDs for the GPU modules in the Nixos system hardware configuration file have been updated. Removed a commented out line for the amdgpuBusId and updated the nvidiaBusId. Added an intelBusId as well.

--- modules/nixos/common/system/hardware/gpu/nvidea/default.nix
+++ modules/nixos/common/system/hardware/gpu/nvidea/default.nix
@@ -55,10 +55,8 @@ in {
             enable = lib.mkOverride 990 true;
             enableOffloadCmd = lib.mkIf config.hardware.nvidia.prime.offload.enable true; # Provides `nvidia-offload` command.
           };
-
-          #TODO: @PETE Check This
-          #amdgpuBusId = "PCI:5:0:0";
-          nvidiaBusId = "PCI:1:0:1";
+          intelBusId = "PCI:8:0:0";
+          nvidiaBusId = "PCI:1:0:0";
         };
       };
 
