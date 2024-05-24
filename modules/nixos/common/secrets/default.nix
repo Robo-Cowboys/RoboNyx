e9eb555187ed552b3386bb4011cc98f10c5d314c
use-the-fork <sincore@gmail.com>
2024-05-24 15:53:46 -0400

    Update encryption keys and modify secret configuration
    This commit updates the encryption keys used in the secrets configuration, specifically for the `auth-key` and `mac` fields. Additionally, adds a new server named "ramen" in the `.sops.yaml` creation rules. Also, it removes `keyFile` and `generateKey` from the `age` module configuration in `default.nix`. Lastly, `lastModified` and `narHash` values in `flake.lock` file are updated to reflect these changes.

--- modules/nixos/common/secrets/default.nix
+++ modules/nixos/common/secrets/default.nix
@@ -8,8 +8,6 @@
       defaultSopsFile = lib.my.getSecretFile "default.yaml";
       age = {
         sshKeyPaths = lib.mkDefault ["/etc/ssh/ssh_host_ed25519_key"];
-        keyFile = "/var/lib/sops-nix/key.txt";
-        generateKey = true;
       };
     };
   };
